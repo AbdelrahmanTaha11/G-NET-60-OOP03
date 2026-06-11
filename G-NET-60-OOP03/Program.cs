4908a1c62331bf8b5895e0769451f2e92eb2c9f2
Abdelrahman Elhalby <abdoelhalby60@gmail.com>
2026-06-11 03:39:21 +0300

    Part01 Question01

--- G-NET-60-OOP03/Program.cs
+++ G-NET-60-OOP03/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace G_NET_60_OOP03
+﻿using System.Reflection.Metadata;
+using System.Security.Principal;
+
+namespace G_NET_60_OOP03
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-          
+            #region Part01
+            #region Question01
+            //Q1: Identify the type of relationship in each scenario below
+
+            //a) A University has Departments. If the university is closed,
+            //    the departments no longer exist. ===> Composition
+
+            //b) A Driver uses a Car. The driver does not own the car ===> Association
+
+            //c) A Dog is an Animal. ==> Inheritance
+
+            //d) A Team has Players. If the team is deleted, the players still exist. ==> Aggregation
+
+            //e) A method receives a Logger as a parameter and calls it inside the method only. ==> Dependency
+
+
+
+            #endregion
+
+            #endregion
         }
     }
 }
